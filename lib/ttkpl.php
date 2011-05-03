b7d271e28b10e44b8e2e445f1949831ebf455db4
David Harker <github.lists@oneiria.co.uk>
2011-05-03 19:29:54 +0100

    added point of entry for refactored code

---
+++ lib/ttkpl.php
@@ -0,0 +1,21 @@
+<?php
+/* 
+ * This file includes all the various bits of the library and provides a point of entry.
+ *
+ */
+
+require_once ("misc_base.php");
+require_once ("scalars.php");
+require_once ("datasource_abstraction.php");
+require_once ("datatype_abstraction.php");
+require_once ("datasource/bintanja_import_normalised.php");
+require_once ("datasource/pmip_import_normalised.php");
+require_once ("math_abstraction.php");
+require_once ("kinetics.php");
+require_once ("time_and_space.php");
+require_once ("thermal_model.php");
+require_once ("thermal_age.php");
+
+
+
+?>
