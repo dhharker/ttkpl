4937a876cc5c7affa3abae069c5b77efef0b41b7
David Harker <github.lists@oneiria.co.uk>
2011-05-07 15:07:54 +0100

    added magic glue ref and section descriptions

--- lib/ttkpl.php
+++ lib/ttkpl.php
@@ -8,6 +8,9 @@
  * @todo write said glue
  */
 
+echo "Begin!\n";
+
+// the abstractions on which the model is built
 require_once 'core/misc_base.php';
 require_once 'core/scalars.php';
 require_once 'core/datasource_abstraction.php';
@@ -15,16 +18,20 @@ require_once 'core/datatype_abstraction.php';
 require_once 'core/math_abstraction.php';
 require_once 'core/kinetics.php';
 require_once 'core/time_and_space.php';
+// the model
 require_once 'core/thermal_model.php';
 require_once 'core/thermal_age.php';
-
+// datasources specific to the model
 require_once 'datasource/bintanja_import_normalised.php';
 require_once 'datasource/pmip_import_normalised.php';
-
-require_once ("external/PHP_GNUPlot.php");
-
+// external libs
+require_once 'external/PHP_GNUPlot.php';
+// for generating nice reports, graphs etc. for use online or in pdfs
 require_once 'reporting/graphs.php';
 require_once 'reporting/report.php';
+// contains funky method-chaining interface to make describing complex samples less of a headache :)
+require_once 'reporting/magic_glue.php';
 
+echo "Finished.\n";
 
 ?>
