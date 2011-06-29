957a576fd9aa9c0627b1ed6153a73895f2e8fd41
David Harker <github.lists@oneiria.co.uk>
2011-06-29 12:24:47 +0100

    TTKPL new released under EUPL 1.1 license

--- lib/reporting/graphs.php
+++ lib/reporting/graphs.php
@@ -1,6 +1,30 @@
 <?php
-/* 
+
+/*
+* Copyright 2008-2011 David Harker
+*  
+* Licensed under the EUPL, Version 1.1 or – as soon they 
+  will be approved by the European Commission - subsequent  
+  versions of the EUPL (the "Licence"); 
+* You may not use this work except in compliance with the 
+  Licence. 
+* You may obtain a copy of the Licence at: 
+*  
+* http://ec.europa.eu/idabc/eupl
+*  
+* Unless required by applicable law or agreed to in 
+  writing, software distributed under the Licence is 
+  distributed on an "AS IS" basis, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+  express or implied. 
+* See the Licence for the specific language governing 
+  permissions and limitations under the Licence. 
+*/ 
+
+
+/**
  * Standardised way of drawing pretty graphs of model i/o for use in web ui and reports
+ * @author David Harker david.harker@york.ac.uk
  */
 
 
--- lib/ttkpl.php
+++ lib/ttkpl.php
@@ -1,11 +1,37 @@
 <?php
-/* 
+
+/*
+* Copyright 2008-2011 David Harker
+*
+* Licensed under the EUPL, Version 1.1 or – as soon they
+  will be approved by the European Commission - subsequent
+  versions of the EUPL (the "Licence");
+* You may not use this work except in compliance with the
+  Licence.
+* You may obtain a copy of the Licence at:
+*
+* http://ec.europa.eu/idabc/eupl
+*
+* Unless required by applicable law or agreed to in
+  writing, software distributed under the Licence is
+  distributed on an "AS IS" basis,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+  express or implied.
+* See the Licence for the specific language governing
+  permissions and limitations under the Licence.
+*/
+
+
+/**
+ *
+ */
+
+/**
  * This file includes all the various bits of the library and provides a point of entry.
  * Include this file in your app to use the library.
  *
- * @todo make somewhere for all the frontend chainable magic glue to go
- * @todo rewrite maths to use arbitrary precision
- * @todo write said glue
+ * @author David Harker david.harker@york.ac.uk
+ * @todo write glue
  */
 
 define ('TTKPL_PATH', realpath (dirname (__FILE__) . '/../') . '/');
