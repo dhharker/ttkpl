c44b03044547a7f1928ab307404d84d037a58920
David Harker <github.lists@oneiria.co.uk>
2012-07-08 12:49:46 +0100

    few error fixes

--- lib/datasource/pmip_import_normalised.php
+++ lib/datasource/pmip_import_normalised.php
@@ -110,7 +110,7 @@ class pmip extends dataSet {
     function getElevationFromFacet (facet $facet) {
         $elev = $this->importer->_extractElevation ($facet->getLat (), $facet->getLon (), $this->varname, $this->timename, $this->modelname);
 
-        $scr = scalarFactory::makeMetres ($this->importer->_getDayMinOffset ($temps, $this->varname), $this);
+        $scr = scalarFactory::makeMetres ($this->importer->_getElevationFromOutput ($temps, $this->varname), $this);
         $td = new temporalDatum ($this->getPalaeoTime (), $scr);
         $sd = new spatialDatum ($facet, $td);
     }
