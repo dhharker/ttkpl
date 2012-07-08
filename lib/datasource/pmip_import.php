b23f8e04d70fc2d2641c614d091ca631096b34fb
David Harker <github.lists@oneiria.co.uk>
2012-07-08 14:22:49 +0100

    removed deprecated _getMaxMin and refactored out most of PMIP2 class to re-use in worldclim importer

--- lib/datasource/pmip_import.php
+++ lib/datasource/pmip_import.php
@@ -31,7 +31,7 @@
 
 
 
-class PMIP2 {
+class PMIP2 extends RawImporter {
     
     const EXTRACT_COMMAND = "/usr/bin/ncks -s \"%%f\\n\" -C -h -d lon,%d.0 -d lat,%d.0 -v %s %s";
     const T_LGM_21KA = "21k";
@@ -54,124 +54,12 @@ class PMIP2 {
         $temps = array ();
         foreach ($times as $t)
             foreach ($vars as $v) {
-                $temps[$t.'a'] = $this->_getMaxMin ($this->_extractTemps ($lat, $lon, $v, $t, self::MODEL_HADCM3M2), $v);
+                $temps[$t.'a'] = $this->_getMaxMinMeanByVarName ($this->_extractTemps ($lat, $lon, $v, $t, self::MODEL_HADCM3M2), $v);
                 //$temps[$t.'a'][$v] = $this->_getMaxMin ($this->_extractTemps ($lat, $lon, $v, $t, self::MODEL_HADCM3M2), $v);
             }
         return $temps;
         
     }
-
-    // deprecated in favour of getMaxMinMeanByVarName
-    function _getMaxMin ($tempArr, $v = self::TMAX_VAR) {
-        sort ($tempArr, SORT_NUMERIC);
-        return ($v == self::TMAX_VAR) ? $tempArr[11] : $tempArr[0];
-    }
-    /**
-     *
-     * @param <type> $tempArr array of monthly temperatures
-     * @param <type> $v self::T(MIN|MAX|MEAN)_VAR determines processing of temps array
-     * @return <type> the max, min or mean of values in the temps array according to the source var name or the whole array if unknown varname.
-     */
-    function _getMaxMinMeanByVarName ($tempArr, $v = self::TMAX_VAR) {
-        sort ($tempArr, SORT_NUMERIC);
-        switch ($v) {
-            case self::TMAX_VAR:
-                return $tempArr[11];
-            case self::TMIN_VAR:
-                return $tempArr[0];
-            case self::TMEAN_VAR:
-                return array_sum($tempArr) / count ($tempArr);
-        }
-        return $tempArr;
-    }
-
-
-    function _getDayMinOffset ($tempArr, $v = self::TMIN_VAR) {
-        $min = $this->_getMaxMin ($tempArr, $v);
-        $mmin = array_filter ($tempArr, function ($v) use (&$min) {
-            return ($v == $min);
-        });
-        $mmin = (count ($mmin) == 1 && $mmin = array_keys($mmin)) ? $mmin[0] : 0;
-        $dmin = round (((365/12) * ($mmin + 1)) + 365/24);
-        return $dmin;
-
-    }
-    
-    function __construct () {
-        $this->dbroot = TTKPL_PATH . 'data/pmip2/';
-        exec ("ls " . $this->dbroot, $this->files);
-    }
-
-
-    function _extractElevation ($lat, $lon, $varname, $timename, $modelname) {
-
-        // This static cache thing is a bit dodgy, but it does speed things up!
-        static $cache = array ();
-
-        $ak = "$lat#$lon#$varname#$timename#$modelname";
-        if (!empty ($cache[$ak])) return $cache[$ak];
-
-        $file = $this->_genDataFileName ($varname, $timename, $modelname);
-
-        $cmd = sprintf (self::EXTRACT_COMMAND, $lon, $lat, $varname, $this->dbroot . $file);
-
-        exec ($cmd, $r);
-        $r = implode("\n", $r);
-
-        $ev = $this->_getElevationFromOutput ($r);
-        $cache[$ak] = $ev;
-
-        return $ev;
-
-    }
-
-    function _extractTemps ($lat, $lon, $varname, $timename, $modelname) {
-
-        // This static cache thing is a bit dodgy, but it does speed things up!
-        static $cache = array ();
-
-        $ak = "$lat#$lon#$varname#$timename#$modelname";
-        if (!empty ($cache[$ak])) return $cache[$ak];
-
-        $file = $this->_genDataFileName ($varname, $timename, $modelname);
-
-        $cmd = sprintf (self::EXTRACT_COMMAND, $lon, $lat, $varname, $this->dbroot . $file);
-
-        exec ($cmd, $r);
-        $r = implode("\n", $r);
-
-        $ts = $this->_getTempsFromOutput ($r);
-        $cache[$ak] = $ts;
-
-        return $ts;
-    }
-    
-    function _getTempsFromOutput ($strin) {
-        $expr = "/\s(\d+\.\d+\s+){1,12}/";
-        if (preg_match ($expr, $strin, $matches) == 0)
-            return false;
-        preg_match_all ("(\d+\.\d+)", $matches[0], $matches);
-        return $matches[0];
-    }
-    
-    function _getElevationFromOutput ($strin) {
-        $expr = '/.*?(\d+\.\d+\s+)\s*$/';
-        if (preg_match ($expr, $strin, $matches) == 0)
-            return false;
-        //preg_match_all ("(\d+\.\d+)", $matches[0], $matches);
-        return $matches[0];
-    }
-
-    function _genDataFileName ($varname, $timename, $modelname) {
-        $expr = "/^$varname.*?$timename.*?$modelname.*?\." . self::NETCDF_DATA_EXT . "$/";
-
-        foreach ($this->files as $f)
-            if (preg_match ($expr, $f) > 0) {
-                $this->filename = $f;
-                return $f;
-            }
-    }
-    
     
     
 }
