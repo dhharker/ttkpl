b6643f2a5ff3c5912263f0a587f3b56c091f8dc2
David Harker <github.lists@oneiria.co.uk>
2012-07-21 22:45:08 +0100

    more debug

--- lib/core/datasource_abstraction.php
+++ lib/core/datasource_abstraction.php
@@ -323,7 +323,7 @@ abstract class csvTimeSeries extends dataSet {
         foreach ($points as $point) {
             $tmp = $this->getRealValueFromFacet ($point);
             if ($tmp === false) {
-                debug ($this->cleanse ($points));
+                debug ($this->cleanse ($facet));
                 debug ($this->cleanse ($tmp));
                 throw new \Exception("Unable to read result!");
                 return false;
